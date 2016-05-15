68be20ea6ef93e33b212d2c74c7a232b96276cd3
Kyle Paul <kpaul86+github@gmail.com>
2016-05-14 21:54:45 -0500

    update(chapter-map): update ChapterMapComponent to enable dynamic zoom, center, markers, & infoWindows

--- src/app/+groups/groups.component.ts
+++ src/app/+groups/groups.component.ts
@@ -10,10 +10,39 @@ import { ChapterMapComponent } from '../chapter-map';
 })
 
 export class GroupsComponent implements OnInit {
+  public zoom:number = 2;
+  public lat:number = 23.5000002;
+  public lng:number = 7.9990339;
+  
+  /*
+  
+  Array of groups goes here using the below model
+  
+  */
+  
+  markers: marker[] = [{
+    lat: 38.9536,
+    lng: -94.7336,
+    info: {
+      chapter: 'GDG Kansas City',
+      link: 'http://www.meetup.com/GDG-Kansas-City/',
+      city: 'Kansas City',
+      country: 'USA'
+    }
+  }];
 
   constructor() {}
 
   ngOnInit() {
   }
 
+}
+
+// just an interface for type safety.
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable?: boolean;
+  info?: any;
 }
--- src/app/chapter-map/chapter-map.component.ts
+++ src/app/chapter-map/chapter-map.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ANGULAR2_GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import {
+  MapsAPILoader,
+  NoOpMapsAPILoader,
+  MouseEvent,
+  ANGULAR2_GOOGLE_MAPS_PROVIDERS,
+  ANGULAR2_GOOGLE_MAPS_DIRECTIVES
+} from 'angular2-google-maps/core';
 
 @Component({
   moduleId: module.id,
@@ -12,14 +19,21 @@ import { ANGULAR2_GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
 export class ChapterMapComponent implements OnInit {
   componentName: 'ChapterMapComponent';
   
-  /* global default */
-  zoom: number = 3;
-  lat: number = 23.5000002;
-  lng: number = 7.9990339;
-
+  @Input() zoom = 2;
+  @Input() lat = 23.5000002;
+  @Input() lng = 7.9990339;
+  @Input() markers = [];
+  
   constructor() {}
 
   ngOnInit() {
   }
-
+  
+  clickedMarker(label: string, index: number) {
+    
+  }
+  
+  mapClicked($event: MouseEvent) {
+    
+  }
 }
