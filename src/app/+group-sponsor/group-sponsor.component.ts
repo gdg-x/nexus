486003b76713705103ca8d36be3666b6d8db16a3
val <valscarlatadev@gmail.com>
2016-05-15 17:27:54 -0500

    formatting on sponsors

--- src/app/+group-sponsor/group-sponsor.component.ts
+++ src/app/+group-sponsor/group-sponsor.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { GroupEventsService } from '../group-events.service';
 import { JSONP_PROVIDERS } from '@angular/http';
 import { GAbout } from '../+group-events/gabout';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 
 @Component({
   moduleId: module.id,
   selector: 'app-group-sponsor',
   templateUrl: 'group-sponsor.component.html',
-  styleUrls: ['group-sponsor.component.css']
+  styleUrls: ['group-sponsor.component.css'],
+  directives: [MdToolbar, MD_CARD_DIRECTIVES]
 })
 export class GroupSponsorComponent implements OnInit {
 
