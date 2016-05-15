5fae9282bf587b3a9a6592faa8f91165b44d8951
Emily Bamsey <em.14@hotmail.com>
2016-05-15 18:33:34 -0400

    Added content to the conduct page

--- src/app/+conduct/conduct.component.ts
+++ src/app/+conduct/conduct.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MdToolbar } from '@angular2-material/toolbar';
 
 @Component({
   moduleId: module.id,
   selector: 'app-conduct',
   templateUrl: 'conduct.component.html',
-  styleUrls: ['conduct.component.css']
+  styleUrls: ['conduct.component.css'],
+  directives: [
+    MdToolbar
+  ]
 })
 export class ConductComponent implements OnInit {
 
